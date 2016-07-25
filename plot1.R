dc1e0f140e85a4468573a4721317a2bd665c3ca2
RyanSuematsu <ryan.suematsu@gmail.com>
2016-07-24 20:19:28 -0700

    updated for pdf

--- plot1.R
+++ plot1.R
@@ -1,3 +1,6 @@
 household_power_consumption <- read.csv("C:/Users/Ryan/Downloads/household_power_consumption.txt", sep=";", na.strings="?")
 data1<-subset(household_power_consumption, as.Date(Date,"%d/%m/%Y")==as.Date('02/01/2007', "%m/%d/%Y") | as.Date(Date,"%d/%m/%Y")==as.Date('02/02/2007', "%m/%d/%Y"), select = Date:Sub_metering_3)
+
+png(file="C:/Users/Ryan/Downloads/plot1.png",width = 480, height = 480)
 hist(data1$Global_active_power,xlab="Global Active Power (kilowatts)",main = "Global Active Power",col = "red")
+dev.off()
